101ca0a9241caa7bc0c774bfcd7b0c36fcb9c0a6
Reshma <reshmasuresh3005@gmail.com>
2024-12-20 09:51:17 +0100

    New Home

---
+++ src/components/HomeFolder.vue
@@ -0,0 +1,32 @@
+<script setup>
+import {onMounted, onUnmounted, ref} from 'vue'
+import CommandPrompt from './CommandPrompt.vue';
+const cmds = ref(['resh ./welcom.sh'])
+function enter() {
+  cmds.value.push('')
+}
+onMounted(() => {
+  window.addEventListener('keyup', enter )
+})
+
+onUnmounted(() => {
+  window.removeEventListener('keyup', enter)
+})
+</script>
+
+<template>
+  <div class="console">
+      <span v-for="cmd in cmds" :key="cmd.id">
+        <CommandPrompt :command="cmd" />
+      </span>
+
+  </div>
+</template>
+
+<style scoped>
+
+.console{
+  display: grid;
+  place-items: left;
+}
+</style>
